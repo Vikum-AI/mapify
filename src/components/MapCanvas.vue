83276910458490d63fc6417c4e88e8b395febd90
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2022-09-20 22:14:59 +0530

    add mapify

---
+++ src/components/MapCanvas.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+const token =
+  "pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww";
+</script>
+
+<template>
+  <mapbox-map
+    accessToken="pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww"
+    mapStyle="light-v10"
+  />
+</template>
