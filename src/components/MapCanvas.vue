4ccf50ec79faef3513f5ba9c6079b4d9435797c3
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2022-09-21 23:35:46 +0530

    Added the pan-to function

--- src/components/MapCanvas.vue
+++ src/components/MapCanvas.vue
@@ -1,15 +1,76 @@
 <script setup lang="ts">
-import { MapboxMap } from "@studiometa/vue-mapbox-gl";
+import { ref } from "vue";
+import {
+  MapboxMap,
+  MapboxGeocoder,
+  MapboxNavigationControl,
+} from "@studiometa/vue-mapbox-gl";
+
+import "@mapbox/mapbox-gl-geocoder/lib/mapbox-gl-geocoder.css";
 </script>
 
 <template>
   <MapboxMap
-    style="height: 100vh; width: 100vw"
+    style="height: 100vh; min-width: 94vw"
     access-token="pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww"
     map-style="mapbox://styles/mapbox/streets-v11"
     :center="[0, 0]"
     :zoom="1"
-  />
+    @mb-created="(mapboxInstance: any) => map = mapboxInstance"
+    map="mb-click"
+  >
+    <MapboxGeocoder />
+    <MapboxNavigationControl position="bottom-right" />
+  </MapboxMap>
+  <button v-on:click="relocateMap()" class="btn-add">add</button>
 </template>
 
 <style></style>
+
+<script lang="ts">
+export default {
+  methods: {
+    relocateMap() {
+      map.value.panTo([114.9631, -80], { duration: 3000 });
+      setTimeout(
+        () =>
+          map.value.zoomTo(5, {
+            duration: 2000,
+            offset: [100, 50],
+          }),
+        3000
+      );
+    },
+  },
+};
+const map = ref();
+
+// if (map.value) map.value.on("click", (e: any) => console.log(e.lngLat));
+
+// map.value?.on("click", (e: any) => {
+//   const clickLocation = e.lngLat;
+//   console.log(clickLocation);
+
+//   map.value.flyTo({
+//     center: clickLocation,
+//   });
+
+// Popup to display latitude and longitude of clicked position so that user can select current position.
+// new mapboxgl.Popup()
+//   .setLngLat(clickLocation)
+//   .setHTML(
+//     "Current Location <br/>Latitude: " +
+//       clickLocation.lat.toFixed(4) +
+//       "<br/>Longitude: " +
+//       clickLocation.lng.toFixed(4)
+//   ) // Text appearing on                                                                                                                                             popup.
+//   .addTo(map);
+
+// if (typeof Storage !== "undefined") {
+//   coordinate = JSON.stringify(clickLocation); // User location stringified and stored to local storage
+//   localStorage.setItem("coordinates", coordinate);
+// } else {
+//   alert("Error: LocalStorage is not supported by current browser.");
+// }
+// });
+</script>
