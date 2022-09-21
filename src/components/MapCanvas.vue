d8e1e7265b9031e672a09c292b8f5e7461f898d0
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2022-09-21 22:32:23 +0530

    Fix map styling + clean code

--- src/components/MapCanvas.vue
+++ src/components/MapCanvas.vue
@@ -1,28 +1,10 @@
-<!-- <script lang="ts">
-const token =
-  "pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww";
-</script>
-
-<template>
-  <mapbox-map
-    accessToken="pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww"
-    mapStyle="light-v10"
-  />
-</template> -->
-
-<script setup>
+<script setup lang="ts">
 import { MapboxMap } from "@studiometa/vue-mapbox-gl";
 </script>
 
 <template>
-  <!-- <div class="map-cont">
-    <MapboxMap
-      access-token="pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww"
-      @mb-created="(mapboxInstance: any) => (map = mapboxInstance)"
-    />
-  </div> -->
   <MapboxMap
-    style="height: 400px; width: 400px"
+    style="height: 100vh; width: 100vw"
     access-token="pk.eyJ1Ijoic2VoYW5kdWsiLCJhIjoiY2w3cmtzOGptMGdiNTN2bnZxemNmajNzaCJ9.ZmuUXZWARzCl1SybKVojww"
     map-style="mapbox://styles/mapbox/streets-v11"
     :center="[0, 0]"
@@ -30,10 +12,4 @@ import { MapboxMap } from "@studiometa/vue-mapbox-gl";
   />
 </template>
 
-<style>
-.map-cont {
-  background-color: red;
-  width: 50vw;
-  height: 50vh;
-}
-</style>
+<style></style>
