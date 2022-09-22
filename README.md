b30da9bc139a4b407ee2b92019bf5738e47eff50
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2022-09-23 00:46:40 +0530

    addded readme

--- README.md
+++ README.md
@@ -17,36 +17,18 @@ If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has a
     2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
 2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
 
-## Customize configuration
+## Start Guide
 
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
+Clone the repo
+``` 
+git clone "https://github.com/Vikum-AI/mapify.git" . 
+```
+Install the packages
+```
+npm i
 ```
 
-### Compile and Hot-Reload for Development
-
-```sh
+Run a dev server
+```
 npm run dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Run Unit Tests with [Vitest](https://vitest.dev/)
-
-```sh
-npm run test:unit
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
 ```
--- src/components/MapCanvas.vue
+++ src/components/MapCanvas.vue
@@ -23,12 +23,20 @@ import "@mapbox/mapbox-gl-geocoder/lib/mapbox-gl-geocoder.css";
     <MapboxNavigationControl position="bottom-right" />
   </MapboxMap>
   <button v-on:click="relocateMap()" class="btn-add">add</button>
+  <button v-on:click="clickMap()">start</button>
 </template>
 
 <style></style>
 
 <script lang="ts">
 export default {
+  data() {
+    return {
+      coordinates: null,
+      marker: "",
+    };
+  },
+
   methods: {
     relocateMap() {
       map.value.panTo([114.9631, -80], { duration: 3000 });
@@ -41,36 +49,54 @@ export default {
         3000
       );
     },
+
+    clickMap() {
+      map.value.on("click", (e: any) => {
+        this.coordinates = e.lngLat;
+        console.log(this.coordinates);
+      });
+    },
+
+    // setMarker() {
+    //   if (this.coordinates !== null)
+    //     this.marker.setLngLat([this.coordinates.lng, this.coordinates.lat]);
+
+    //   console.log(this.coordinates);
+    // },
+
+    addPoint(coordinates: any) {
+      map.value.Popup().setLngLat(coordinates);
+    },
   },
 };
 const map = ref();
 
-// if (map.value) map.value.on("click", (e: any) => console.log(e.lngLat));
+if (map.value) map.value.on("click", (e: any) => console.log(e.lngLat));
 
-// map.value?.on("click", (e: any) => {
-//   const clickLocation = e.lngLat;
-//   console.log(clickLocation);
+map.value?.on("click", (e: any) => {
+  const clickLocation = e.lngLat;
+  console.log(clickLocation);
 
-//   map.value.flyTo({
-//     center: clickLocation,
-//   });
+  map.value.flyTo({
+    center: clickLocation,
+  });
 
-// Popup to display latitude and longitude of clicked position so that user can select current position.
-// new mapboxgl.Popup()
-//   .setLngLat(clickLocation)
-//   .setHTML(
-//     "Current Location <br/>Latitude: " +
-//       clickLocation.lat.toFixed(4) +
-//       "<br/>Longitude: " +
-//       clickLocation.lng.toFixed(4)
-//   ) // Text appearing on                                                                                                                                             popup.
-//   .addTo(map);
+  // Popup to display latitude and longitude of clicked position so that user can select current position.
+  // new mapboxgl.Popup()
+  //   .setLngLat(clickLocation)
+  //   .setHTML(
+  //     "Current Location <br/>Latitude: " +
+  //       clickLocation.lat.toFixed(4) +
+  //       "<br/>Longitude: " +
+  //       clickLocation.lng.toFixed(4)
+  //   ) // Text appearing on                                                                                                                                             popup.
+  //   .addTo(map);
 
-// if (typeof Storage !== "undefined") {
-//   coordinate = JSON.stringify(clickLocation); // User location stringified and stored to local storage
-//   localStorage.setItem("coordinates", coordinate);
-// } else {
-//   alert("Error: LocalStorage is not supported by current browser.");
-// }
-// });
+  // if (typeof Storage !== "undefined") {
+  //   coordinate = JSON.stringify(clickLocation); // User location stringified and stored to local storage
+  //   localStorage.setItem("coordinates", coordinate);
+  // } else {
+  //   alert("Error: LocalStorage is not supported by current browser.");
+  // }
+});
 </script>
